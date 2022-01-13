569b79190fa130486ca06dfda84a3c7821345bff
hsl4125 <qlhuangrui@gmail.com>
2022-01-14 00:01:02 +0800

    reading variables

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -126,6 +126,7 @@ static void       printStatement();
 static void       expressionStatement();
 static void       declaration();
 static void       varDeclaration();
+static uint8_t    identifierConstant(Token *name);
 static ParseRule *getRule(TokenType type);
 static void       parsePrecedence(Precedence precedence);
 
@@ -216,6 +217,13 @@ static void string() {
         copyString(parser.previous.start + 1, parser.previous.length - 2)));
 }
 
+static void namedVariable(Token name) {
+    uint8_t arg = identifierConstant(&name);
+    emitBytes(OP_GET_GLOBAL, arg);
+}
+
+static void variable() { namedVariable(parser.previous); }
+
 static void unary() {
     TokenType operatorType = parser.previous.type;
 
@@ -253,7 +261,7 @@ ParseRule rules[] = {
     [TOKEN_GREATER_EQUAL] = {NULL, binary, PREC_COMPARISON},
     [TOKEN_LESS]          = {NULL, binary, PREC_COMPARISON},
     [TOKEN_LESS_EQUAL]    = {NULL, binary, PREC_COMPARISON},
-    [TOKEN_IDENTIFIER]    = {NULL, NULL, PREC_NONE},
+    [TOKEN_IDENTIFIER]    = {variable, NULL, PREC_NONE},
     [TOKEN_STRING]        = {string, NULL, PREC_NONE},
     [TOKEN_NUMBER]        = {number, NULL, PREC_NONE},
     [TOKEN_AND]           = {NULL, NULL, PREC_NONE},
