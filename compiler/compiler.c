4949b07e5416a8bd533c842023ebb7cc4059d88b
hsl4125 <qlhuangrui@gmail.com>
2022-01-14 19:14:53 +0800

    for statement

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -173,6 +173,7 @@ static void       expression();
 static void       statement();
 static void       ifStatement();
 static void       whileStatement();
+static void       forStatement();
 static void       printStatement();
 static void       expressionStatement();
 static void       declaration();
@@ -554,6 +555,8 @@ static void statement() {
         ifStatement();
     } else if (match(TOKEN_WHILE)) {
         whileStatement();
+    } else if (match(TOKEN_FOR)) {
+        forStatement();
     } else if (match(TOKEN_LEFT_BRACE)) {
         beginScope();
         block();
@@ -612,6 +615,56 @@ static void whileStatement() {
 
     patchJump(exitJump);
     emitByte(OP_POP);
+}
+
+// TODO: refactor
+static void forStatement() {
+    beginScope();
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+    // for-initializer
+    if (match(TOKEN_SEMICOLON)) {
+        // No initializer.
+    } else if (match(TOKEN_VAR)) {
+        varDeclaration();
+    } else {
+        expressionStatement();
+    }
+
+    // for-exit
+    int loopStart = currentChunk()->count;
+    int exitJump  = -1;
+    if (!match(TOKEN_SEMICOLON)) {
+        expression();
+        consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+
+        // Jump out of the loop if condition is false.
+        exitJump = emitJump(OP_JUMP_IF_FALSE);
+        emitByte(OP_POP);
+    }
+
+    // for-increment
+    if (!match(TOKEN_RIGHT_PAREN)) {
+        int bodyJump = emitJump(OP_JUMP);
+        emitByte(OP_POP);
+        int incrementStart = currentChunk()->count;
+
+        expression();
+        consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clause.");
+
+        emitLoop(loopStart);
+        loopStart = incrementStart;
+        patchJump(bodyJump);
+    }
+
+    statement();
+    emitLoop(loopStart);
+
+    if (exitJump != -1) {
+        patchJump(exitJump);
+        emitByte(OP_POP);
+    }
+
+    endScope();
 }
 
 bool compile(const char *source, Chunk *chunk) {
