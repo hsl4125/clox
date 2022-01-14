c57609591a3d0cd2aa30deae048a48d2287e2418
hsl4125 <qlhuangrui@gmail.com>
2022-01-14 13:09:41 +0800

    using locals var

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -157,6 +157,7 @@ static void       printStatement();
 static void       expressionStatement();
 static void       declaration();
 static void       varDeclaration();
+static int        resolveLocal(Compiler *compiler, Token *name);
 static uint8_t    identifierConstant(Token *name);
 static ParseRule *getRule(TokenType type);
 static void       parsePrecedence(Precedence precedence);
@@ -249,13 +250,22 @@ static void string(bool canAssign) {
 }
 
 static void namedVariable(Token name, bool canAssign) {
-    uint8_t arg = identifierConstant(&name);
+    uint8_t getOp, setOp;
+    int     arg = resolveLocal(current, &name);
+    if (arg != -1) {
+        getOp = OP_GET_LOCAL;
+        setOp = OP_SET_LOCAL;
+    } else {
+        arg   = identifierConstant(&name);
+        getOp = OP_GET_GLOBAL;
+        setOp = OP_SET_GLOBAL;
+    }
 
     if (canAssign && match(TOKEN_EQUAL)) {
         expression();
-        emitBytes(OP_SET_GLOBAL, arg);
+        emitBytes(setOp, (uint8_t) arg);
     } else {
-        emitBytes(OP_GET_GLOBAL, arg);
+        emitBytes(getOp, (uint8_t) arg);
     }
 }
 
@@ -356,6 +366,20 @@ static bool identifiersEqual(Token *a, Token *b) {
     return memcmp(a->start, b->start, a->length) == 0;
 }
 
+static int resolveLocal(Compiler *compiler, Token *name) {
+    for (int i = compiler->localCount - 1; i > 0; --i) {
+        Local *local = &compiler->locals[ i ];
+        if (identifiersEqual(name, &local->name)) {
+            if (local->depth == -1) {
+                error("Can't read local variable in its own initilizer.");
+            }
+            return i;
+        }
+    }
+
+    return -1;
+}
+
 static void addLocal(Token name) {
     if (current->localCount == UINT8_MAX) {
         error("Too many local variables in function");
@@ -364,7 +388,7 @@ static void addLocal(Token name) {
 
     Local *local = &current->locals[ current->localCount++ ];
     local->name  = name;
-    local->depth = current->scopeDepth;
+    local->depth = -1; // uninitialized
 }
 
 static void declareVariable() {
@@ -387,8 +411,13 @@ static void declareVariable() {
     addLocal(*name);
 }
 
+static void markInitialized() {
+    current->locals[ current->localCount - 1 ].depth = current->scopeDepth;
+}
+
 static void defineVariable(uint8_t global) {
     if (current->scopeDepth > 0) {
+        markInitialized();
         return;
     }
 
